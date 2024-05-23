e0adfdf764d09f7462297c0c2c779435d8e89885
otofune <otofune@otofune.me>
2024-05-23 15:38:19 +0900

    Support relative path

--- main.ts
+++ main.ts
@@ -19,17 +19,16 @@ const DEFAULT_SETTINGS: PluginSettings = {
 
 class VerticalLinesPluginValue implements PluginValue {
 	constructor(
-		private path: string,
 		private view: EditorView,
-		private callback: Function,
+		private callback: (dom: HTMLElement) => void,
 
 	) {
-		// console.log(this.view);
+		console.log(this.view);
 	}
 
 	update(update: ViewUpdate) {
 		// console.log("update", update);
-		this.callback(this.view.dom, this.path);
+		this.callback(this.view.dom);
 
 		// if (
 		// 	update.docChanged ||
@@ -54,20 +53,19 @@ export default class HtmlLocalImgPlugin extends Plugin {
 			this.processElement(element, ctx.sourcePath);
 		})
 
-
-
-		let activeFile = this.app.workspace.getActiveFile();
-		if (activeFile) {
-			this.registerEditorExtension(
-				ViewPlugin.define(
-					(view) =>
-						new VerticalLinesPluginValue(
-							activeFile?.path,
-							view,
-							this.processElement.bind(this)
-						)),
-			);
-		}
+		this.registerEditorExtension(
+			ViewPlugin.define(
+				(view) =>
+					new VerticalLinesPluginValue(
+						view,
+						dom => {
+							const af = this.app?.workspace.getActiveFile()
+							if (af) {
+								this.processElement(dom,af.path)
+							}
+						}
+					)),
+		);
 	}
 
 	processElement(element: HTMLElement, sourcePath: string) {
@@ -83,6 +81,9 @@ export default class HtmlLocalImgPlugin extends Plugin {
 				continue;
 			}
 			let clean_link = link.src.replace('app://obsidian.md/', '')
+			if (link.attributes.getNamedItem('src')!.value.startsWith('.')) {
+				clean_link = link.attributes.getNamedItem('src')!.value
+			}
 			// console.log('clean_link: ' + clean_link)
 
 			let imageFile = this.app.metadataCache.getFirstLinkpathDest(clean_link, sourcePath);
@@ -100,6 +101,7 @@ export default class HtmlLocalImgPlugin extends Plugin {
 			let full_link = active_path + '/' + clean_link
 			// console.log('full_link: ' + full_link)
 			link.src = full_link
+			link.style.maxWidth = "100%"
 			if (Platform.isMobile) {
 				console.log("Running on mobile platform - setting object fit and height of img")
 				link.style.objectFit = "contain"
